8d65334b09df55e5055899a198cb5b3cbc203977
Diego Raffa <diego170975@gmail.com>
2025-07-28 01:08:44 +0100

    added hero section for mobile first and then adapt for desktop, added also the funcionality to change header opacity on scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+// HAMBURGER MENU
 const mobileToggle = document.querySelector("#mobile-menu-toggle");
 const mobileList = document.querySelector(".mobile-menu-list");
 
 mobileToggle.addEventListener("click", (e) => {
   mobileList.classList.toggle("active");
+});
+
+// REDUCE HEADER OPACITY ON SCROLL
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", (e) => {
+  navbar.style.opacity = Math.max(1 - scrollY / 300, 0.8);
 });
